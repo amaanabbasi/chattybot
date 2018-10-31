be7bc54b67fe0f2bb5732862e4775b8b7ce8e092
amaanabbasi <amaanabbasi99@gmail.com>
2018-11-01 00:01:13 +0530

    cool

--- main.py
+++ main.py
@@ -11,36 +11,37 @@ def start():
 
     if sys.argv[1] == 'train' and sys.argv[2]:
 
-        cleaned_chat = cleanText(os.path.join(os.getcwd(),'chat_data','raw_chat_data',sys.argv[2]))
+        cleaned_chat = cleanText(os.path.join(os.getcwd(),sys.argv[2]))
         filename = os.path.join('chat_data','cleaned_chat.txt')
 
+        # Saving the cleaned chat disk.
         with open(filename, 'w', encoding="utf-8") as f:
             for lines in cleaned_chat:
                 f.write("%s\n" % lines)
         
         chatbot = trainBot(filename)
-        print("traing complete chatbot:{chatbot} trained.")
+        print("traing complete chatbot:{chatbot} trained.\n Type in: python main.py chat. ")
         sys.exit(1)
 
     elif sys.argv[1] == 'chat':
         engine = pyttsx3.init()
         chatbot = ChatBot('Amaan')
+
         while True:
             con = str(input("You: "))
             response = chatbot.get_response(str(con))
             print(f"Bot: {response}")
             engine.say(f"{response}")
             engine.runAndWait()
-    
+   	 
     elif sys.argv[1] == 'api':
     	os.system('python app.py') 
 
     else :
-        print ("for training: python main.py train <Input TextFileName> \nfor chat : python main.py chat")
+        print ("for training: python main.py train <Input TextFileName> \nTo chat : python main.py chat")
         sys.exit(1)
     
 
-if __name__ == "__main__":
-    
+if __name__ == "__main__":   
     start()
-   	
+   
--- myfunctions/train.py
+++ myfunctions/train.py
@@ -5,18 +5,17 @@ def trainBot(filename):
     ''' Training on the cleaned chat. '''
     chat_data = []
 
+    # Loading chat data into a list
     with open(filename, 'r', encoding='utf-8', errors='ignore') as infile:
         for line in infile:
             chat_data.append(line.strip()) # strip to remove /n characters
             
-    # Intializing the bot
+    # Intializing the bot and then training it on the data.
     chatbot = ChatBot('Amaan')
-
     conversation =  chat_data
-    
-    #
     chatbot.set_trainer(ListTrainer)
     chatbot.train(conversation)
 
+    # Returning the trained bot 
     return chatbot
 
